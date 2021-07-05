18ab2e01889b8bae57b43fba061a4c9f74aade59
pedrokkl <77468171+pedrokkl@users.noreply.github.com>
2021-07-05 15:32:05 -0300

    delete
    delete

--- 28/main.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-image = cv2.imread('image.jpg')
-grayscale = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-
-imagem_processada = cv2.adaptiveThreshold(grayscale, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-cv2.imshow('resultado',imagem_processada)
-cv2.waitKey(0)
